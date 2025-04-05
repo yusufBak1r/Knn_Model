076833bb4df1fb5bd35dcda5490502b75de504f1
yusuf <yusuf.bak1r3637@gmail.com>
2025-04-05 22:27:31 +0300

    .

---
+++ src/main/main.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self):
+        print("MyClass sınıfı başlatıldı!")
+
+    def greet(self):
+        print("Merhaba!")
+
+def main():
+    my_object = MyClass()
+    my_object.greet()
+
+if __name__ == "__main__":
+    main()
